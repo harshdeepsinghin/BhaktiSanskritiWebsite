82988346ba2dd9dc00c388fbc2e1fc6df7b272db
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2025-07-30 15:40:05 +0530

    fix: Update base path in Vite config for BhaktiSanskritiWebsite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/BhaktiSanskritiWebsite/',
   server: {
     host: "::",
     port: 8080,
