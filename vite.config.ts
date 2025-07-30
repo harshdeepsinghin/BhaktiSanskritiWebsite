364cbe0c3b435e94f8550db2a2287ae839d17435
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2025-07-30 15:45:54 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,6 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/',
-  server: {
-    host: "::",
-    port: 80,
-  },
   plugins: [
     react(),
   ].filter(Boolean),
