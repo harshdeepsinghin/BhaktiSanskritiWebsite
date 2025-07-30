9724472d258521ecd03d5dd358a90676a80243b9
Harshdeep Singh <94488766+harshdeepsinghin@users.noreply.github.com>
2025-07-30 15:25:05 +0530

    feat: Add GitHub Actions workflow for deploying Vite site to GitHub Pages; update base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/BhaktiSanskritiWebsite/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-
   ].filter(Boolean),
   resolve: {
     alias: {
